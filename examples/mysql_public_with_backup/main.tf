e6fcdb24796662a39fab6ef1a45dcdcf4285e46a
Manon <manonf@padok.fr>
2022-10-21 17:44:29 +0200

    feat: add example for mysql public with backup

---
+++ examples/mysql_public_with_backup/main.tf
@@ -0,0 +1,75 @@
+# Example of code for deploying a private MySQL DB with a peering between your private subnet and cloudsql service.
+# To access to your DB, you need a bastion or a VPN connection from your client.
+locals {
+  project_id = "padok-cloud-factory"
+}
+
+provider "google" {
+  project = local.project_id
+  region  = "europe-west3"
+}
+
+provider "google-beta" {
+  project = local.project_id
+  region  = "europe-west3"
+}
+
+module "my-network" {
+  source = "git@github.com:padok-team/terraform-google-network.git?ref=v3.0.0"
+
+  name       = "my-network-6"
+  project_id = local.project_id
+
+  subnets = {
+    "my-private-subnet-3" = {
+      name             = "my-private-subnet-6"
+      region           = "europe-west3"
+      primary_cidr     = "10.35.0.0/16"
+      serverless_cidr  = ""
+      secondary_ranges = {}
+    }
+  }
+  gcp_peering_cidr = "10.0.22.0/24"
+}
+
+module "my-sql-exporter" {
+  source = "../../modules/sql-exporter"
+
+  name = "my-exporter-1"
+
+  project_id = local.project_id
+  region     = "europe-west3"
+}
+
+module "my-public-mysql-db-with-backup" {
+  source = "../../modules/mysql"
+
+  name              = "my-public-mysql-db2" # Mandatory
+  engine_version    = "MYSQL_8_0"           # Mandatory
+  project_id        = local.project_id      # Mandatory
+  region            = "europe-west1"        # Mandatory
+  availability_type = "ZONAL"
+
+  disk_limit = 20
+
+  users          = ["User_1", "User_2"]
+  create_secrets = true
+
+  backup_configuration = {
+    enabled  = true
+    location = "europe-west3"
+  }
+
+  databases = ["MYDB_1"]
+
+  private_network = module.my-network.network_id
+
+  sql_exporter = {
+    bucket_name  = module.my-sql-exporter.bucket_name
+    pubsub_topic = module.my-sql-exporter.pubsub_topic
+    schedule     = "0 2 * * *"
+    timezone     = "UTC"
+  }
+
+  public = true
+}
---
+++ examples/mysql_public_with_backup/outputs.tf
@@ -0,0 +1,30 @@
+output "instance_name" {
+  value       = module.my-public-mysql-db-with-backup.instance_name
+  description = "The instance name for the master instance"
+}
+
+output "instance_connection_name" {
+  value       = module.my-public-mysql-db-with-backup.instance_connection_name
+  description = "The connection name of the master instance to be used in connection strings"
+}
+
+output "read_replica_instance_names" {
+  value       = module.my-public-mysql-db-with-backup.read_replica_instance_names
+  description = "The instance names for the read replica instances"
+}
+
+output "private_ip_address" {
+  description = "The first private (PRIVATE) IPv4 address assigned for the master instance"
+  value       = module.my-public-mysql-db-with-backup.private_ip_address
+}
+
+output "users" {
+  description = "List of maps of additional users and passwords"
+  value = [for r in module.my-public-mysql-db-with-backup.users :
+    {
+      name     = r.name
+      password = r.password
+    }
+  ]
+  sensitive = true
+}
