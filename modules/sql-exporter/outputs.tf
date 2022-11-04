ac6a4f21269558c3899a732cbb4fbe87d8b8d50a
Manon <manonf@padok.fr>
2022-11-04 15:59:24 +0100

    fix: sql-export output bucket name

--- modules/sql-exporter/outputs.tf
+++ modules/sql-exporter/outputs.tf
@@ -1,6 +1,6 @@
 output "bucket_name" {
   description = "The name of the bucket in which export files will be kept"
-  value       = google_storage_bucket.this
+  value       = google_storage_bucket.this.name
 }
 
 output "pubsub_topic" {
