6837a8c3688e433e4f6446009084514f11cc5f93
Alan <alanl@padok.fr>
2023-02-28 18:31:27 +0100

    fix(encryption): change in resource resulted in issue

--- modules/encryption/main.tf
+++ modules/encryption/main.tf
@@ -12,9 +12,7 @@ resource "google_kms_crypto_key_iam_member" "crypto_key" {
   crypto_key_id = var.encryption_key_id == null ? google_kms_crypto_key.this[0].id : var.encryption_key_id
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
 
-  members = [
-    "serviceAccount:${google_project_service_identity.gcp_sa_cloud_sql.email}",
-  ]
+  member = "serviceAccount:${google_project_service_identity.gcp_sa_cloud_sql.email}"
 }
 
 # Create key if not provided
