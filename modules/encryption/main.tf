6a2662ffde457ac97a8547e08ab5c831a99654e2
Alan-pad <46325799+Alan-pad@users.noreply.github.com>
2023-03-02 10:59:21 +0100

    fix(encryption): missing project_id/project variable in submodule (#32)
    * fix(encryption): resources in encryption module did not set the project variable
    
    * fix(encryption): google_kms_crypto_key does not support project attribute
    
    * fix(encryption): iam resources for KMS don't support project attribute
    
    * fix(encryption): change in resource resulted in issue

--- modules/encryption/main.tf
+++ modules/encryption/main.tf
@@ -1,24 +1,25 @@
 # You need to create a service account for each project that requires customer-managed encryption keys.
 resource "google_project_service_identity" "gcp_sa_cloud_sql" {
   provider = google-beta
+  project  = var.project_id
   service  = "sqladmin.googleapis.com"
 }
 
 # Grant access to the key
-resource "google_kms_crypto_key_iam_binding" "crypto_key" {
-  provider      = google-beta
+resource "google_kms_crypto_key_iam_member" "crypto_key" {
+  provider = google-beta
+
   crypto_key_id = var.encryption_key_id == null ? google_kms_crypto_key.this[0].id : var.encryption_key_id
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
 
-  members = [
-    "serviceAccount:${google_project_service_identity.gcp_sa_cloud_sql.email}",
-  ]
+  member = "serviceAccount:${google_project_service_identity.gcp_sa_cloud_sql.email}"
 }
 
-# Create key if not provided 
+# Create key if not provided
 resource "google_kms_key_ring" "this" {
   count = var.encryption_key_id == null ? 1 : 0
 
+  project  = var.project_id
   name     = var.name
   location = var.region
 }
