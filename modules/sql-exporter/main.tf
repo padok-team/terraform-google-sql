c86c4f8c73897fe81117348812bafaeeabfb694b
CallumHemsley <bythepenguin@gmail.com>
2023-04-07 17:38:35 +0200

    chore: skip GCP_78 and GCP_114 in sql exporter

--- modules/sql-exporter/main.tf
+++ modules/sql-exporter/main.tf
@@ -60,6 +60,9 @@ resource "random_id" "suffix" {
 
 
 module "function" {
+  #checkov:skip=CKV_GCP_78:Ensure Cloud storage has versioning enabled
+  #checkov:skip=CKV_GCP_114:Ensure public access prevention is enforced on Cloud Storage bucket
+  # Skipped because it doesn't need to be an option in the module below.
   source  = "terraform-google-modules/event-function/google"
   version = "~> 2.5.0"
 
